0cfbbf93c06d940b74e5b5780fccdb388364b772
Marco <marco@localhost.localdomain>
2018-11-26 19:36:01 +0100

    Added name and changed message

--- Program/Hello.java
+++ Program/Hello.java
@@ -1,5 +1,7 @@
-public class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello world");
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Knock knock, " + args[0]);
     }
+
 }

7d4b24ad4b2f361b9a739a83a87e8782037eb02a
marco <marco@localhost.localdomain>
2018-11-30 20:03:19 +0100

    testing

--- dunno.js
+++ dunno.js
@@ -1,15 +1,17 @@
 function fibonacci(n) {
     switch (n){
-    case 0:
-        return 0;
-
-    case 1:
-        return 1;
-
-    default:
-        return fibonacci(n - 2) + fibonacci(n - 1);
-
+    case 0: return 0;
+    case 1: return 1;
+    default: return fibonacci(n - 2) + fibonacci(n - 1);
     }
 }
 
+new_var = function (integer) {
+    // return Math.floor(integer); 
+    
+    return Math.floor.apply(integer); 
+};
+
 console.log(fibonacci(6));
+
+console.log(new_var(3.23)); 

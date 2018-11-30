490455f0a2eeab1d0564efc0ef90cd0eeb35fefc
marco <marco@localhost.localdomain>
2018-11-30 19:54:03 +0100

    Fibonacci function added

--- dunno.js
+++ dunno.js
@@ -1,3 +1,15 @@
-var hello = "ciao"; 
+function fibonacci(n) {
+    switch (n){
+    case 0:
+        return 0;
 
-console.log(hello); 
+    case 1:
+        return 1;
+
+    default:
+        return fibonacci(n - 2) + fibonacci(n - 1);
+
+    }
+}
+
+console.log(fibonacci(6));

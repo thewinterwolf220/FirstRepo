1c58e6e52cdd3068b54e1cfdbb42e5af5fe7b57d
marco <m4st3r0fsh4d0w@protonmail.com>
2018-12-01 22:50:23 +0100

    Refactored and added new useful method

--- MyCalendar/src/com/phoenix/Wizards.java
+++ MyCalendar/src/com/phoenix/Wizards.java
@@ -11,6 +11,14 @@ import static com.phoenix.Factory.*;
 class Wizards {
     private static Scanner reader = new Scanner(System.in);
 
+
+    static Calendar timeWizard() {
+        System.out.println("Date: hh mm [dd MM YYYY]");
+        String time = reader.nextLine();
+        return Utils.createTime(Utils.lineToTime(time));
+    }
+
+
     static iEvent eventWizard() {
         System.out.println("Activity: ");
         String activity = reader.nextLine();
@@ -18,10 +26,7 @@ class Wizards {
         System.out.println("Place: ");
         String place = reader.nextLine();
 
-        System.out.println("Date: hh mm [dd MM YYYY]");
-        String time = reader.nextLine();
-
-        Calendar date = Utils.createTime(Utils.lineToTime(time));
+        Calendar date = timeWizard();
 
         System.out.println("Details? ");
         String details = reader.nextLine();
@@ -37,7 +42,6 @@ class Wizards {
         String time = reader.nextLine();
 
         Calendar end = Utils.createTime(Utils.lineToTime(time));
-
         return buildEvent(tmp, end);
     }
 
@@ -51,9 +55,6 @@ class Wizards {
 
         return buildTask(tmp, Task.getPriority(Integer.valueOf(priority)));
     }
-
-
-    // TODO: 28/11/18 Write Object version
 
     static iConference conferenceWizard() {
         System.out.println("Speaker of the conference: ");

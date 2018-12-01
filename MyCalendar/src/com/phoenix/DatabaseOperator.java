acaf8ffafad9ed24d50e2baee58c030b7306d90e
marco <m4st3r0fsh4d0w@protonmail.com>
2018-12-01 16:43:59 +0100

    Removed unused imports and unused variables. Removed redundant comments.
     Introduced variables to manage login, which logic has been improved
    
     Renamed fetchall for a more significant name.

--- MyCalendar/src/com/phoenix/DatabaseOperator.java
+++ MyCalendar/src/com/phoenix/DatabaseOperator.java
@@ -1,140 +1,137 @@
 package com.phoenix;
 
 import com.phoenix.calendar.Task;
-import jdk.jshell.execution.Util;
-import jdk.jshell.spi.ExecutionControlProvider;
-import org.jetbrains.annotations.NotNull;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
-
-import static com.phoenix.Main.keyboard;
 
 class DatabaseOperator {
 
-
-    private static boolean DEBUG = true;
-
-    // We want their scope to be the whole runtime.
+    private static final int MAX_ATTEMPTS = 3;
+    private static PGSimpleDataSource source = new PGSimpleDataSource();
+    private static int attempts = 0;
     private static String username = null;
     private static String password = null;
+    private static boolean logged = false;
 
+    static List<iEvent> allFutureEvents() {
+        List<iEvent> all = new ArrayList<>();
 
-    // Test here
-    public static void main(String[] args) {
-        Comparator<iEvent> comparator = Comparator.comparing(iEvent::getCalendar);
-        Stream.of(fetchAll()).forEachOrdered(System.out::println);
-    }
-    //-------------------------------------Methods to get stuff --------------------------------------------------------
-
-    static void upcoming() {
-        if (countEvents() > 0) upcomingEvents();
-        else System.out.println("No upcoming events");
-
-        if (countTasks() > 0) upcomingTasks();
-        else System.out.println("No upcoming tasks");
-
-        if (countDurEvents() > 0) upcomingDurableEvents();
-        else System.out.println("No upcoming events");
-
-    }
-
-
-    private static List<iEvent> fetchAll() {
-        List<iEvent> eventList = new ArrayList<>();
+        String[] tables = getTables().toArray(new String[]{});
 
         try (Connection connection = connectToDatabase()) {
+            for (String table : tables) {
 
-            try (Statement statement = connection.createStatement()) {
+                String query = prepareStatement(table);
 
-                String select = "SELECT activity, place, time_and_date, details " +
-                        "FROM events WHERE time_and_date > clock_timestamp()";
-
-                statement.executeQuery(select);
-
-                ResultSet resultSet = statement.getResultSet();
-
-                while (resultSet.next()) {
-                    iEvent event = Utils.fetchEvent(resultSet);
-                    eventList.add(event);
+                try (Statement statement = connection.createStatement()) {
+                    statement.executeQuery(query);
+                    ResultSet set = statement.getResultSet();
+                    while (set.next()) {
+                        iEvent fetched = Utils.fetchGeneric(table, set);
+                        all.add(fetched);
+                    }
                 }
             }
-
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return eventList;
+        all.sort(Comparator.comparing(iEvent::getCalendar));
+        return all;
     }
 
-    private static void upcomingEvents() {
+    static boolean addEvent() {
+
+        // To add an event, we must first ask for the attributes, which are inserted via the Wizards class.
+        // These are converted to the corresponding types in postgresql and added to the database
+        // The same occurs, but with variations according to the object type, in others adding methods
+
+        ArrayList<Object> attributes = Wizards.eventWizard().getAttributes();
+
+        Timestamp timeOfEvent = Utils.calendarToTimestamp(attributes.get(2));
 
         try (Connection connection = connectToDatabase()) {
             try (Statement statement = connection.createStatement()) {
 
-                String select = "SELECT activity, place, time_and_date, details " +
-                        "FROM events WHERE time_and_date > clock_timestamp()";
+                String insert = "INSERT INTO events(activity, place, time_and_date, details) VALUES " +
+                        "(" +
+                        "'" + attributes.get(0) + "'," +
+                        "'" + attributes.get(1) + "'," +
+                        "'" + timeOfEvent + "'," +
+                        "'" + attributes.get(3) + "'" +
+                        ")";
+                statement.execute(insert);
 
-                statement.executeQuery(select);
-                ResultSet resultSet = statement.getResultSet();
+                System.out.println("Added a new task");
 
-                while (resultSet.next()) {
-                    iEvent fetchedEvent = Utils.fetchEvent(resultSet);
-                    System.out.println(fetchedEvent);
-                }
+                return true;
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception exx) {
+            return false;
         }
     }
 
-    private static void upcomingTasks() {
+    static boolean addTask() {
+
+        ArrayList<Object> attributes = Wizards.taskWizard().getAttributes();
+
+        Timestamp timeOfEvent = Utils.calendarToTimestamp(attributes.get(2));
+
+        //Don't be scared, it's just a cast operation.
+        int priority = ((Task.Priority) attributes.get(4)).getImportance();
 
         try (Connection connection = connectToDatabase()) {
             try (Statement statement = connection.createStatement()) {
 
-                String s = "SELECT activity, place, time_and_date, details, priority " +
-                        "FROM tasks WHERE time_and_date > clock_timestamp()";
-                statement.executeQuery(s);
+                String insert = "INSERT INTO tasks(activity, place, time_and_date, details, priority) VALUES" +
+                        "( " +
+                        "'" + attributes.get(0) + "'," +
+                        "'" + attributes.get(1) + "'," +
+                        "'" + timeOfEvent + "'," +
+                        "'" + attributes.get(3) + "'," +
+                        "'" + priority + "'" +
+                        ")";
 
-                ResultSet resultSet = statement.getResultSet();
+                statement.execute(insert);
+                System.out.println("Added a new task");
 
-                while (resultSet.next()) {
-                    iTask fetchedTask = Utils.fetchTask(resultSet);
-                    System.out.println(fetchedTask);
-                }
+                return true;
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception exx) {
+            return false;
         }
     }
 
-    private static void upcomingDurableEvents() {
+    static boolean addDurableEvent() {
+        ArrayList<Object> attributes = Wizards.durableEventWizard().getAttributes();
+
+        Timestamp timeOfEvent = Utils.calendarToTimestamp(attributes.get(2));
+        Timestamp endOfEvent = Utils.calendarToTimestamp(attributes.get(4));
+
         try (Connection connection = connectToDatabase()) {
             try (Statement statement = connection.createStatement()) {
 
-                String s = "SELECT activity, place, time_and_date, details, end_time " +
-                        "FROM durable_events WHERE time_and_date > clock_timestamp()";
-                statement.executeQuery(s);
+                String insert = "INSERT INTO durable_events(activity, place, time_and_date, details, end_time) VALUES" +
+                        "( " +
+                        "'" + attributes.get(0) + "'," +
+                        "'" + attributes.get(1) + "'," +
+                        "'" + timeOfEvent + "'," +
+                        "'" + attributes.get(3) + "'," +
+                        "'" + endOfEvent + "'" +
+                        ")";
+                statement.execute(insert);
+                System.out.println("Added a new event");
 
-                ResultSet resultSet = statement.getResultSet();
-
-                while (resultSet.next()) {
-                    iDurableEvent fetchedDurableEvent = Utils.fetchDurableEvent(resultSet);
-                    System.out.println(fetchedDurableEvent);
-                }
+                return true;
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception exx) {
+            return false;
         }
     }
-
-
-    // ---------------------------Counters of future elements-----------------------------------------------
 
     private static int countEvents() {
         int count = 0;
@@ -184,137 +181,98 @@ class DatabaseOperator {
         return count;
 
     }
-    //------------------------------------------------------------------------------------------------------------------
 
-
-    //------------------------------Methods to insert into the database-------------------------------------------------
-
-    static void addEvent() {
-
-        // To add an event, we must first ask for the attributes, which are inserted via the Wizards class.
-        // These are converted to the corresponding types in postgresql and added to the database
-        // The same occurs, but with variations according to the object type, in others adding methods
-
-        ArrayList<Object> attributes = Wizards.eventWizard().getAttributes();
-
-        Timestamp timeOfEvent = Utils.calendarToTimestamp(attributes.get(2));
-
+    private static int countTables() {
+        int count = 0;
         try (Connection connection = connectToDatabase()) {
-            try (Statement statement = connection.createStatement()) {
 
-                String insert = "INSERT INTO events(activity, place, time_and_date, details) VALUES " +
-                        "(" +
-                        "'" + attributes.get(0) + "'," +
-                        "'" + attributes.get(1) + "'," +
-                        "'" + timeOfEvent + "'," +
-                        "'" + attributes.get(3) + "'" +
-                        ")";
-                boolean result = statement.execute(insert);
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet res = metaData.getTables(null, null, null, new String[]{"TABLE"});
 
-                if (!result) System.out.println("Added a new event");
-            }
-        } catch (Exception exx) {
-            exx.printStackTrace();
-        }
-    }
+            while (res.next())
+                count++;
 
-    static void addTask() {
-
-        ArrayList<Object> attributes = Wizards.taskWizard().getAttributes();
-
-        Timestamp timeOfEvent = Utils.calendarToTimestamp(attributes.get(2));
-
-        //Don't be scared, it's just a cast operation.
-        int priority = ((Task.Priority) attributes.get(4)).getImportance();
-
-        try (Connection connection = connectToDatabase()) {
-            try (Statement statement = connection.createStatement()) {
-
-                String insert = "INSERT INTO tasks(activity, place, time_and_date, details, priority) VALUES" +
-                        "( " +
-                        "'" + attributes.get(0) + "'," +
-                        "'" + attributes.get(1) + "'," +
-                        "'" + timeOfEvent + "'," +
-                        "'" + attributes.get(3) + "'," +
-                        "'" + priority + "'" +
-                        ")";
-
-                statement.execute(insert);
-                System.out.println("Added a new task");
-            }
-        } catch (Exception exx) {
-            exx.printStackTrace();
-        }
-    }
-
-    static void addDurableEvent() {
-        ArrayList<Object> attributes = Wizards.durableEventWizard().getAttributes();
-
-        Timestamp timeOfEvent = Utils.calendarToTimestamp(attributes.get(2));
-        Timestamp endOfEvent = Utils.calendarToTimestamp(attributes.get(4));
-
-        try (Connection connection = connectToDatabase()) {
-            try (Statement statement = connection.createStatement()) {
-
-                String insert = "INSERT INTO durable_events(activity, place, time_and_date, details, end_time) VALUES" +
-                        "( " +
-                        "'" + attributes.get(0) + "'," +
-                        "'" + attributes.get(1) + "'," +
-                        "'" + timeOfEvent + "'," +
-                        "'" + attributes.get(3) + "'," +
-                        "'" + endOfEvent + "'" +
-                        ")";
-                statement.execute(insert);
-
-                System.out.println("Added a new event");
-            }
-        } catch (Exception exx) {
-            exx.printStackTrace();
-        }
-    }
-
-
-    //-------------------------------------------------------------------------------------------------------------------
-    private static Connection connectToDatabase() {
-        PGSimpleDataSource source = new PGSimpleDataSource();
-        source.setServerName("localhost");
-        source.setDatabaseName("calendar");
-        source.setLoginTimeout(10);
-
-        // The login should be asked only if credentials are not initialised.
-        // TODO: 29/11/18 Improve logic, refactor.
-        if (username == null && password == null)
-            login(source);
-        else {
-            source.setUser(username);
-            source.setPassword(password);
-        }
-
-        try {
-            return source.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
-            System.exit(1);
-            return null;                // Dead code.
+        }
+        return count;
+    }
+
+    private static List<String> getTables() {
+        List<String> tables = new ArrayList<>(countTables());
+
+        try (Connection connection = connectToDatabase()) {
+
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet res = metaData.getTables(null, null, null, new String[]{"TABLE"});
+
+            while (res.next())
+                tables.add(res.getString("TABLE_NAME"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tables;
+    }
+
+    private static String prepareStatement(String table) {
+        switch (table) {
+            case "events":
+                return "SELECT activity, place, time_and_date, details FROM events " +
+                        "WHERE time_and_date > clock_timestamp()";
+            case "durable_events":
+                return "SELECT activity, place, time_and_date, details, end_time FROM durable_events " +
+                        "WHERE time_and_date > clock_timestamp()";
+            case "tasks":
+                return "SELECT activity, place, time_and_date, details, priority FROM tasks " +
+                        "WHERE time_and_date > clock_timestamp()";
+            default:
+                throw new UnsupportedOperationException();
         }
     }
 
-    // Auxiliary to connectToDatabase()
-    // It changes user and password of the parameter object.
+    private static Connection connectToDatabase() {
+        source.setServerName("localhost");
+        source.setDatabaseName("calendar");
 
-    private static void login(@NotNull PGSimpleDataSource dataSource) {
-        keyboard.nextLine();
+        if (logged) { // Reuses username and password previously inserted
+            source.setUser(username);
+            source.setPassword(password);
+        } else login();
 
-        System.out.println("Insert username: ");
-        username = keyboard.nextLine();
+        try {
+            logged = true;
+            return source.getConnection();
+        } catch (SQLException e) {
+            attempts++;
+            logged = false;
+            System.out.println("Wrong credentials, retry.");
 
-        System.out.println("Insert password: ");
-        password = keyboard.nextLine();
+            if (attempts > MAX_ATTEMPTS) {
+                System.out.println("You can't retry");
+                return null;
+            }
 
-        dataSource.setUser(username);
-        dataSource.setPassword(password);
+            return connectToDatabase();
+        }
     }
 
+    private static void login() {
+        System.out.println("User ");
+        username = Main.keyboard.nextLine();
+
+        System.out.println("Password: ");
+        password = Main.keyboard.nextLine();
+
+        source.setUser(username);
+        source.setPassword(password);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(logged ? "Logged" : "Not logged");
+        connectToDatabase();
+        System.out.println(logged ? "Logged" : "Not logged");
+    }
 
 }
 

bbb369e8838e78d932c97b9005187508840ae6ca
Marco <m4st3r0fsh4d0w@protonmail.com>
2018-12-02 17:04:35 +0100

    Now the connection needs to be established only when the program is started

--- MyCalendar/src/com/phoenix/Main.java
+++ MyCalendar/src/com/phoenix/Main.java
@@ -48,6 +48,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        DatabaseOperator.establishConnection();
         String code;
         do {
             printOperations();
